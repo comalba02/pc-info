bb12a86d453e3480d5e7f32820124c374cd2f4ce
Marco Coronado <comalba02@hotmail.com>
2022-10-29 16:49:10 -0500

    se crea el diseño UI

--- src/pc/info/PcInfo.java
+++ src/pc/info/PcInfo.java
@@ -1,9 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package pc.info;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -15,7 +15,8 @@ public class PcInfo {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+
+        
     }
-    
+
 }
